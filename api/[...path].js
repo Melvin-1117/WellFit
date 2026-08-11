b51e2bcb59b02f0a96baa4b4d74c57774eae84e6
Melvimn-1117 <antonymelvin81@gmail.com>
2026-08-11 22:01:59 +0530

    Reconstruct req.url from Vercel catch-all req.query.path parameter

--- api/[...path].js
+++ api/[...path].js
@@ -1,3 +1,9 @@
 const app = require("../backend/server");
 
-module.exports = app;
+module.exports = (req, res) => {
+  if (req.query && req.query.path) {
+    const p = Array.isArray(req.query.path) ? req.query.path.join("/") : req.query.path;
+    req.url = "/api/" + p;
+  }
+  return app(req, res);
+};
